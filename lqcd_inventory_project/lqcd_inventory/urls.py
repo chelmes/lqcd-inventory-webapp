02af2e8bd74b2bf27536fa9ebce698223fb75630
Martin Ueding <dev@martin-ueding.de>
2016-11-07 20:29:43 +0100

    Add some generic views

--- lqcd_inventory_project/lqcd_inventory/urls.py
+++ lqcd_inventory_project/lqcd_inventory/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^perambulators/create$', views.perambulators_create, name='perambulators_create'),
-    url(r'^perambulators/(?:P<perambulator_id>\d+)/view$', views.perambulators_view, name='perambulators_view'),
+    url(r'^perambulators/(?P<pk>\d+)/view$', views.PerambulatorView.as_view(), name='perambulators_view'),
     url(r'^perambulators/$', views.perambulators_index, name='perambulators_index'),
+    #url(r'^dilution_schemes/create$', views.dilution_schemes_create, name='dilution_schemes_create'),
+    url(r'^dilution_schemes/(?P<pk>\d+)/view$', views.DilutionSchemeView.as_view(), name='dilution_schemes_view'),
+    #url(r'^dilution_schemes/$', views.dilution_schemes_index, name='dilution_schemes_index'),
+    #url(r'^ensembles/create$', views.ensembles_create, name='ensembles_create'),
+    url(r'^ensembles/(?P<pk>\d+)/view$', views.EnsembleView.as_view(), name='ensembles_view'),
+    #url(r'^ensembles/$', views.ensembles_index, name='ensembles_index'),
     url(r'^$', views.index, name='index'),
 ]
--- lqcd_inventory_project/lqcd_inventory/views.py
+++ lqcd_inventory_project/lqcd_inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
 
 from . import models
 
@@ -24,5 +25,16 @@ def perambulators_create(request):
     return render(request, 'lqcd_inventory/404.html')
 
 
-def perambulators_view(request, perambulator_id):
-    return render(request, 'lqcd_inventory/404.html')
+class PerambulatorView(generic.DetailView):
+    model = models.Perambulator
+    template_name = 'lqcd_inventory/perambulators_view.html'
+
+
+class DilutionSchemeView(generic.DetailView):
+    model = models.DilutionScheme
+    template_name = 'lqcd_inventory/dilution_schemes_view.html'
+
+
+class EnsembleView(generic.DetailView):
+    model = models.Ensemble
+    template_name = 'lqcd_inventory/ensembles_view.html'
