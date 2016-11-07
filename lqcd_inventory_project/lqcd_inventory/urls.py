1d4620949e17b9d2fb50bbaf6b9c4afe65f2c39a
Martin Ueding <dev@martin-ueding.de>
2016-11-07 18:36:11 +0100

    Add some templates

--- lqcd_inventory_project/lqcd_inventory/urls.py
+++ lqcd_inventory_project/lqcd_inventory/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^parambulators/create$', views.parambulators_create, name='parambulators_create'),
+    url(r'^parambulators/(?:P<parambulator_id>\d+)/view$', views.parambulators_view, name='parambulators_view'),
+    url(r'^parambulators/$', views.parambulators_index, name='parambulators_index'),
     url(r'^$', views.index, name='index'),
 ]
--- lqcd_inventory_project/lqcd_inventory/views.py
+++ lqcd_inventory_project/lqcd_inventory/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
+
 def index(request):
-    return HttpResponse("Hi")
+    return render(request, 'lqcd_inventory/index.html')
+
+
+def parambulators_index(request):
+    parambulator_list = models.Perambulator.objects
+
+    return render(
+        request,
+        'lqcd_inventory/parambulators_index.html',
+        perambulator_list=perambulator_list,
+    )
+
+
+def parambulators_create(request):
+    return render(request, 'lqcd_inventory/404.html')
+
+
+def parambulators_view(request, parambulator_id):
+    return render(request, 'lqcd_inventory/404.html')
