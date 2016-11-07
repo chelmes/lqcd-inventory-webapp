c078048fca756b01e04b22ecb0ccc81c62f7e0b9
Martin Ueding <dev@martin-ueding.de>
2016-11-07 17:30:11 +0100

    Add some model

--- lqcd_inventory_project/lqcd_inventory/models.py
+++ lqcd_inventory_project/lqcd_inventory/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class DilutionScheme(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Ensemble(models.Model):
+    mu_l = models.FloatField()
+
+
+class Eigensystem(models.Model):
+    path = models.CharField(max_length=1000)
+
+    ensemble = models.ForeignKey(Ensemble, on_delete=models.CASCADE)
+
+
+class Perambulator(models.Model):
+    random_seed = models.IntegerField()
+    mass_light = models.FloatField()
+    mass_strange = models.FloatField()
+    mass_charm = models.FloatField()
+
+    ensemble = models.ForeignKey(Ensemble, on_delete=models.CASCADE)
+    dilution_scheme = models.ForeignKey(DilutionScheme, on_delete=models.CASCADE)
+
--- lqcd_inventory_project/lqcd_inventory_project/settings.py
+++ lqcd_inventory_project/lqcd_inventory_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lqcd_inventory.apps.LqcdInventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
